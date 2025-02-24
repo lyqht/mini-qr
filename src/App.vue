b1274f9a42fe8cdeeba7e31b371d3c865e8a8ea3
David <david@firerock.sg>
2025-02-24 18:42:04 +0800

    put icon into language selector

--- src/App.vue
+++ src/App.vue
@@ -624,26 +624,6 @@ async function generateBatchQRCodes(format: 'png' | 'svg') {
                 </svg>
               </span>
             </button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon"
-              width="36"
-              height="36"
-              viewBox="0 0 24 24"
-            >
-              <g
-                fill="none"
-                stroke="#abcbca"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-              >
-                <path d="M4 5h7M7 4c0 4.846 0 7 .5 8" />
-                <path
-                  d="M10 8.5c0 2.286-2 4.5-3.5 4.5S4 11.865 4 11c0-2 1-3 3-3s5 .57 5 2.857c0 1.524-.667 2.571-2 3.143m2 6l4-9l4 9m-.9-2h-6.2"
-                />
-              </g>
-            </svg>
             <Combobox
               :items="locales"
               v-model:value="locale"
--- src/components/ui/Combobox/Combobox.vue
+++ src/components/ui/Combobox/Combobox.vue
@@ -31,10 +31,31 @@ defineProps<{
         role="combobox"
         :aria-expanded="open"
         :aria-label="buttonLabel"
-        class="w-fit justify-between focus-visible:ring-1 bg-zinc-50 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-900 dark:hover:text-zinc-100"
+        class="w-fit flex gap-2 items-center justify-between focus-visible:ring-1 bg-zinc-50 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-900 dark:hover:text-zinc-100"
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon -ml-1.5 size-6 shrink-0"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="#abcbca"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M4 5h7M7 4c0 4.846 0 7 .5 8" />
+            <path
+              d="M10 8.5c0 2.286-2 4.5-3.5 4.5S4 11.865 4 11c0-2 1-3 3-3s5 .57 5 2.857c0 1.524-.667 2.571-2 3.143m2 6l4-9l4 9m-.9-2h-6.2"
+            />
+          </g>
+        </svg>
+
         {{ value ? items.find((item) => item.value === value)?.label : 'Select item...' }}
-        <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        <ChevronsUpDown class="size-4 shrink-0 opacity-50" />
       </Button>
     </PopoverTrigger>
     <PopoverContent class="w-fit p-0">
