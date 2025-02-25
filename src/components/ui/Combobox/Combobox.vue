3234227c1058c868e18718d425635e10a4c30d3e
David <david@firerock.sg>
2025-02-25 11:35:08 +0800

    replace the icon with a named slot

--- src/components/ui/Combobox/Combobox.vue
+++ src/components/ui/Combobox/Combobox.vue
@@ -33,27 +33,7 @@ defineProps<{
         :aria-label="buttonLabel"
         class="w-fit flex gap-2 items-center justify-between focus-visible:ring-1 bg-zinc-50 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-900 dark:hover:text-zinc-100"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon -ml-1.5 size-6 shrink-0"
-          width="36"
-          height="36"
-          viewBox="0 0 24 24"
-        >
-          <g
-            fill="none"
-            stroke="#abcbca"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          >
-            <path d="M4 5h7M7 4c0 4.846 0 7 .5 8" />
-            <path
-              d="M10 8.5c0 2.286-2 4.5-3.5 4.5S4 11.865 4 11c0-2 1-3 3-3s5 .57 5 2.857c0 1.524-.667 2.571-2 3.143m2 6l4-9l4 9m-.9-2h-6.2"
-            />
-          </g>
-        </svg>
-
+        <slot name="button-icon"></slot>
         {{ value ? items.find((item) => item.value === value)?.label : 'Select item...' }}
         <ChevronsUpDown class="size-4 shrink-0 opacity-50" />
       </Button>
