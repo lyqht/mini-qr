606385da22e9cd3fb37dbac9c64f4a843b82819f
lyqht <estee.tsw@gmail.com>
2025-05-09 19:11:30 +0800

    pwa automatic reload

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import { i18n } from './utils/i18n'
 import App from './App.vue'
 import './index.css'
 import './style.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createApp(App).use(i18n).mount('#app')
